c0c816540100459fd61bd72d305031a8e75a31de
steve88k <abcsteveabc@hotmail.com>
2026-06-03 21:12:58 +0800

    Revert "backend"
    This reverts commit 550817ad42186334ff9e6ed28812817f013bc2b9.

--- backend/src/routes/predictRoutes.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import {getPrediction} from '../controllers/predictController';
-
-const router = Router();
-
-router.get('/predict', getPrediction);
-
-export default router;
