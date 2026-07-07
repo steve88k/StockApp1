2233e626fd13e62676eb00e79765c55ef921434f
steve88k <abcsteveabc@hotmail.com>
2026-07-08 02:48:22 +0800

    Revert "update metro"
    This reverts commit 5affa7c153717e2aef7ba9049fa95c1cfc78c5f4.

--- metro.config.js
+++ metro.config.js
@@ -6,12 +6,6 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
+const config = {};
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-const config = {resolver: {
-    assetExts: [...defaultConfig.resolver.assetExts, 'tflite'],
-  },
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
